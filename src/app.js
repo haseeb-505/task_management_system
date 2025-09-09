3acb67c726d0b80c5cb29334090ebac7890159fd
haseeb-505 <hatahir87626@gmail.com>
2025-09-09 17:57:54 +0500

    auth routes are configured in app.js file

--- src/app.js
+++ src/app.js
@@ -5,13 +5,25 @@ import cookierParser from "cookie-parser";
 
 const app = express();
 
+// data handling
+app.use(express.json({limit: "16Kb"}));
+
+// url encoder
+app.use(express.urlencoded({extended: true, limit: "16Kb"}));
+
+// static folder
+app.use(express.static("public"));
+
 // cookierParser
 app.use(cookierParser());
 
 
-// Define a route
+// Define or import routes here and then use them
+import authRoutes from "./routes/authRoutes.js"
 app.get('/', (req, res) => {
 res.send('Hello, Express!');
 });
+
+app.use("/api/v1/auth", authRoutes);
 
 export {app}
