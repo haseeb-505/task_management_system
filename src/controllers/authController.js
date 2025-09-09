7a75daef84f350aba10ea6c8a496dfa8f6e9d7e1
haseeb-505 <hatahir87626@gmail.com>
2025-09-09 17:49:13 +0500

    registerUser and loginUser controllers added

---
+++ src/controllers/authController.js
@@ -0,0 +1,19 @@
+import * as authService from "../services/authService.js";
+
+export const registerUser = async (req, res) => {
+    try {
+        const result = await authService.register(req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+};
+
+export const login = async (req, res) => {
+  try {
+    const result = await authService.login(req.body);
+    res.json(result);
+  } catch (err) {
+    res.status(401).json({ message: err.message });
+  }
+};
