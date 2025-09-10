a718e52a650fc82f200bd96e6c4ea042ebaf4de4
haseeb-505 <hatahir87626@gmail.com>
2025-09-10 18:51:40 +0500

    task Routes are added and few are tested

---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { 
+    createTask, 
+    getTasks, 
+    getTaskById, 
+    updateTask, 
+    deleteTask, 
+    uploadTaskFiles,
+    assignTask, 
+    getUnassignedTasks, 
+    getCompanyUsers
+} from "../controllers/taskController.js";
+import { verifyJWT, authorize } from "../middlewares/authMiddleware.js";
+import { upload } from "../middlewares/multerMiddleware.js";
+
+const router = express.Router();
+
+// Task routes
+router.post("/create-task", verifyJWT, authorize(['SuperAdmin', 'CompanyUser', 'EndUser']), createTask);
+router.get("/get-tasks", verifyJWT, authorize([]), getTasks);
+router.get("/get-task/:id", verifyJWT, authorize([]), getTaskById);
+router.put("/update-task/:id", verifyJWT, authorize([]), updateTask);
+router.delete("/delete-task/:id", verifyJWT, authorize([]), deleteTask);
+
+// File upload route - mark task as completed
+router.post("/complete-task/:id/upload", 
+    verifyJWT, 
+    authorize([]), 
+    upload.array('files', 5), // Max 5 files
+    uploadTaskFiles
+);
+
+// Super Admin only routes for task assignment
+router.get('/task-assignment/unassigned', verifyJWT, authorize(['SuperAdmin']), getUnassignedTasks);
+router.get('/task-assignment/company-users', verifyJWT, authorize(['SuperAdmin']), getCompanyUsers);
+router.patch('/task-assignment/:taskId/assign', verifyJWT, authorize(['SuperAdmin']), assignTask);
+
+export default router;
